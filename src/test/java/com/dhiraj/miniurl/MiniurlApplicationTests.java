7ddbdf02ca741cbaa0498e1d0a03efad49952d2c
Dhiraj <dkskp2005@gmail.com>
2025-12-21 17:36:07 +0530

    chore : init

---
+++ src/test/java/com/dhiraj/miniurl/MiniurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhiraj.miniurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
