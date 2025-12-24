f0594fc1c5d21cd64d0c3f18704ba6b805e37e70
Dhiraj <dkskp2005@gmail.com>
2025-12-24 23:39:19 +0530

    cors,env

--- src/main/java/com/dhiraj/miniurl/config/SecurityConfig.java
+++ src/main/java/com/dhiraj/miniurl/config/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.dhiraj.miniurl.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+//    for cors
+    @Value("${app.cors.origins}")
+    private String allowedOrigins;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -17,9 +29,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->
                         auth.anyRequest().permitAll());
         return http.build();
+    }
+
+//    Global CORS config
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+//        split comma seperated origin into a list
+        List<String> origins = Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim).toList();
+        config.setAllowedOrigins(origins);
+
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);          // Important if using cookies or JWTs
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply to all endpoints
+        return source;
     }
 }
--- src/main/java/com/dhiraj/miniurl/controller/UrlShortenerController.java
+++ src/main/java/com/dhiraj/miniurl/controller/UrlShortenerController.java
@@ -2,19 +2,25 @@ package com.dhiraj.miniurl.controller;
 
 import com.dhiraj.miniurl.dto.ShortenUrlRequest;
 import com.dhiraj.miniurl.dto.ShortenUrlResponse;
+import com.dhiraj.miniurl.exception.ShortUrlNotFoundException;
 import com.dhiraj.miniurl.service.UrlShortenerService;
 import com.dhiraj.miniurl.util.AnonymousUserUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class UrlShortenerController {
+
+    @Value("${app.base-url}")
+    private String baseUrl;
 
     private final UrlShortenerService urlShortenerService;
 
@@ -32,10 +38,24 @@ public class UrlShortenerController {
         String shortCode =
                 urlShortenerService.shortenUrl(request.getOriginalUrl(), anonKey);
 
-        String shortUrl = "http://localhost:8080/" + shortCode;
+//        String shortUrl = "http://localhost:8080/" + shortCode;
+            String shortUrl = baseUrl +"/"+ shortCode;
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ShortenUrlResponse(shortUrl));
     }
+
+
+    @GetMapping("/expand")
+    public ResponseEntity<Map<String, String>> expandUrl(@RequestParam String code) {
+        try {
+            String originalUrl = urlShortenerService.getOriginalUrl(code);
+            return ResponseEntity.ok(Map.of("originalUrl", originalUrl));
+        } catch (ShortUrlNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("error", "Short URL not found"));
+        }
+    }
+
 }
